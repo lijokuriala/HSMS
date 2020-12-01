3c3522c294d452d3536f7768e15efbab5144304a
Lijo <lijo.net@gmail.com>
2020-11-30 21:45:40 -0600

    State checking

--- bin/hsms.py
+++ bin/hsms.py
@@ -22,8 +22,11 @@ def get_garage_door_state(pin):
     return state
 
 
-class main_hsms:
+class HSMS:
     """ Main functions of HSMS"""
+
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
 
     def main(self):
 
@@ -34,8 +37,30 @@ class main_hsms:
             self.logger.info("Status of door is \"%s\"", get_garage_door_state(15))
             logging.info(("Status of door is \"%s\"", get_garage_door_state(15)))
 
+            state = get_garage_door_state(15)
+            while True:
+                # Last state of each garage door
+                door_states = dict()
+
+                # time.time() of the last time the garage door changed state
+                time_of_last_state_change = dict()
+
+                door_states["MainGarage"] = get_garage_door_state(15)
+                time_of_last_state_change["MainGarage"] = time.time()
+
+                if door_states["MainGarage"] != state:
+                    door_states["MainGarage"] = state
+                    time_of_last_state_change["MainGarage"] = time.time()
+                    self.logger.info("State changed to %s",state)
+
         except KeyboardInterrupt:
             logging.critical("Terminating due to keyboard interrupt")
         except:
             logging.critical("Terminating due to unexpected error: %s", sys.exc_info()[0])
             logging.critical("%s", traceback.format_exc())
+
+        GPIO.cleanup()
+
+
+if __name__ == "__main__":
+    HSMS().main()
