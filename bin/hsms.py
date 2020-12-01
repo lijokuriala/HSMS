04bfd5c7768a71964eed61eea99cca2f94acb59a
Lijo <lijo.net@gmail.com>
2020-11-30 19:06:48 -0600

    Init commit and push

---
+++ bin/hsms.py
@@ -0,0 +1,36 @@
+import sys
+import time
+import logging
+import traceback
+
+import RPi.GPIO as GPIO
+import gdoor_config as gdoorcfg
+
+
+# Get Sensor data
+def get_garage_door_state(pin):
+    if GPIO.input(pin):
+        state = 'Open'
+    else:
+        state = 'Closed'
+
+    return state
+
+
+class main_hsms:
+    """ Main functions of HSMS"""
+
+    def main(self):
+
+        try:
+            # Configure sensor pis
+            for gdoor in gdoorcfg.GARAGE_DOORS:
+                self.logger.info("Configuring pin %d for \"%s\"", gdoor['pin'], gdoor['name'])
+
+        except KeyboardInterrupt:
+            logging.critical("Terminating due to keyboard interrupt")
+        except:
+            logging.critical("Terminating due to unexpected error: %s", sys.exc_info()[0])
+            logging.critical("%s", traceback.format_exc())
+
+
---
+++ etc/gdoor_config.py
@@ -0,0 +1,12 @@
+# List and configure all garage doors
+GARAGE_DOORS = [
+    {
+        'pin': 15,
+        'name': "Main Garage Door"
+    },
+
+    # {
+    #     'pin': 16,
+    #     'name': "Test Garage Door"
+    # }
+]
