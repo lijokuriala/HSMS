2eed0c991f46deec9527669f040f599667544a0f
Lijo <lijo.net@gmail.com>
2020-11-30 21:28:10 -0600

    Test

--- bin/hsms.py
+++ bin/hsms.py
@@ -7,7 +7,9 @@ import logging
 import traceback
 
 import RPi.GPIO as GPIO
-import gdoor_config as gdoorcfg
+
+
+# import gdoor_config as gdoorcfg
 
 
 # Get Sensor data
@@ -27,13 +29,12 @@ class main_hsms:
 
         try:
             # Configure sensor pis
-            for gdoor in gdoorcfg.GARAGE_DOORS:
-                self.logger.info("Configuring pin %d for \"%s\"", gdoor['pin'], gdoor['name'])
+            #           for gdoor in gdoorcfg.GARAGE_DOORS:
+            #                self.logger.info("Configuring pin %d for \"%s\"", gdoor['pin'], gdoor['name'])
+            self.logger.info("Status of door is \"%s\"", get_garage_door_state(15))
 
         except KeyboardInterrupt:
             logging.critical("Terminating due to keyboard interrupt")
         except:
             logging.critical("Terminating due to unexpected error: %s", sys.exc_info()[0])
             logging.critical("%s", traceback.format_exc())
-
-
