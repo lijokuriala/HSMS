47fb75e597f1bf559dbb0274dc413ac0d1f058bf
Lijo <lijo.net@gmail.com>
2020-12-02 17:36:34 -0600

    Time debug3

--- bin/hsms.py
+++ bin/hsms.py
@@ -124,6 +124,7 @@ class HSMS:
                 time_no_change = time.time() - time_of_last_state_change[sensor_name]
                 self.logger.info("Time no change  %0.f sec", time_no_change)
                 if time_no_change >= 15:
+                    self.logger.info("time_no_change >= 15  %0.f sec", time_no_change)
                     if time_no_change % 15 == 0:
                         self.logger.info("No change in status for 15 seconds now #30 minutes now")
                     if time_no_change % 18 == 0:
