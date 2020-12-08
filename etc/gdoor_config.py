641e77eef15fe6798b59e4dd1ce88b6ee284447f
Lijo <lijo.net@gmail.com>
2020-12-07 23:08:19 -0600

    Update sensor config2

--- etc/gdoor_config.py
+++ etc/gdoor_config.py
@@ -4,7 +4,7 @@ SENSORS = [
         'pin': 15,
         'name': "GARAGE Door",
         'alert_notify': 'Y',
-        'alert_notification_interval_minutes': 0.5
+        'alert_notification_interval_minutes': 0.3
     },
 
      {
