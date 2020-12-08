3d19a36a44091064b51bc4cc277a0e3b53e2026d
Lijo <lijo.net@gmail.com>
2020-12-07 22:49:19 -0600

    Config changes for alert duration

--- etc/gdoor_config.py
+++ etc/gdoor_config.py
@@ -3,12 +3,14 @@ SENSORS = [
     {
         'pin': 15,
         'name': "Main Garage Door",
-        'alert_notify': 'Y'
+        'alert_notify': 'Y',
+        'alert_notification_interval_minutes': 0.25
     },
 
      {
          'pin': 40,
          'name': "Test Garage Door",
-         'alert_notify': 'N'
+         'alert_notify': 'N',
+        'alert_notification_interval_minutes': 0.5
      }
 ]
